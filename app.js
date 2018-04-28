77179a85da9c37255c3a38f52205f9742e4b75d4
jfranklin1607 <jfranklin1607@gmail.com>
2018-04-28 11:44:02 -0400

    express

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var port = 3000;
+
+var app = express();
+
+app.use(function(require, res, next){
+	console.log('Time: ', Date.now());
+	next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+	res.send('Hello World!');
+});
+
+app.get('/about', function(req, res){
+	res.send('About Page');
+});
+
+app.listen(3000);
+console.log('Server started on port 3000');
+
+module.exports = app;
